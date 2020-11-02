140f56d020c7c60ba285858d0703925bc8992ae7
Rachana <rachanarrachu28@gmail.com>
2020-11-03 00:00:34 +0530

    Refactored : Code refactored using comparable extends

--- src/main/java/findingmaximum/FindingMaximum.java
+++ src/main/java/findingmaximum/FindingMaximum.java
@@ -14,37 +14,20 @@ public class FindingMaximum {
 		return o1.compareTo(o2);
 	}
 
-	public static int maxInteger(Integer a, Integer b, Integer c) {
-		int max = a;
-
-		if (b.compareTo(a) > 0)
-			max = b;
-		if (c.compareTo(b) > 0)
-			max = c;
-		return max;
-	}
-
-	public static float maxFloat(Float a, Float b, Float c) {
-		float max = a;
-		if (b.compareTo(a) > 0.0)
-			max = b;
-		if (c.compareTo(b) > 0.0)
-			max = c;
-		return max;
-	}
-
-	public static String maxString(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(a) > 0)
-			max = b;
-		if (c.compareTo(b) > 0)
-			max = c;
-		return max;
-	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("****WELCOME TO FIND MAXIMUM AMONG GIVEN SET****");
-		System.out.println("Maximum integer" + maxInteger(1000, 400, 22));
-		System.out.println("Maximum float" + maxFloat(2.3f, 7.8f, 9.0f));
-		System.out.println("Maximum float" + maxString("Apple","Peach","Banana"));
+		System.out.println("Maximum integer" +maximum(1000, 400, 22));
+		System.out.println("Maximum float" + maximum(2.3f, 7.8f, 9.0f));
+		System.out.println("Maximum String" + maximum("Apple","Peach","Banana"));
+	}
+		private static <T extends Comparable <T>>T maximum(T a,T b,T c){
+			T max=a;
+		if (b.compareTo(a) > 0)
+			max = b;
+		if (c.compareTo(b) > 0)
+			max = c;
+		return max;
 	}
 }
