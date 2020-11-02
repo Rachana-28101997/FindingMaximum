8adf0cc4f5fe864e2f88f18abfb0c6528d225aec
Rachana <rachanarrachu28@gmail.com>
2020-11-02 22:45:59 +0530

    Added : Maximum out off 3 integers and test cases in each position

--- src/main/java/findingmaximum/FindingMaximum.java
+++ src/main/java/findingmaximum/FindingMaximum.java
@@ -1,7 +1,22 @@
 package findingmaximum;
 
 public class FindingMaximum {
+	
+	public int compare(Integer o1, Integer o2) {
+		return o1.compareTo(o2);
+	}
+
+	public static int maxInteger(Integer a, Integer b, Integer c) {
+		int max = a;
+
+		if (b.compareTo(a) > 0)
+			max = b;
+		if (c.compareTo(b) > 0)
+			max = c;
+		return max;
+	}
 public static void main(String[] args) {
 	System.out.println("****WELCOME TO FIND MAXIMUM AMONG GIVEN SET****");
+	System.out.println("Maximum integer" +maxInteger(1000,400,22));
 }
 }
--- src/test/java/findingmaximum/TestFindingMaximum.java
+++ src/test/java/findingmaximum/TestFindingMaximum.java
@@ -1,5 +1,32 @@
 package findingmaximum;
-
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import findingmaximum.FindingMaximum;
 public class TestFindingMaximum {
+	static FindingMaximum maximum;
+@BeforeClass
+public static void findMaximumObj() {
+	maximum = new FindingMaximum();
+	System.out.println("In Before class");
+}
+
+@Test
+public void testMaximumAtFirstPosition_testAssertionHappy() {
+	int result = maximum.maxInteger(500, 200, 30);
+	Assert.assertEquals(500,result);
+}
+
+@Test
+public void testMaximumAtSecondPosition_testAssertionHappy() {
+	int result = maximum.maxInteger(101, 222, 38);
+	Assert.assertEquals(222,result);
+}
+
+@Test
+public void testMaximumAtThirdPosition_testAssertionHappy() {
+	int result = maximum.maxInteger(1000, 267, 2987);
+	Assert.assertEquals(2987,result);
+}
 
 }
