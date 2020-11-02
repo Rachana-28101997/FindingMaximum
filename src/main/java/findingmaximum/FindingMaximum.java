970c076131a27d7e0be56d3d8be61e6c90793ca3
Rachana <rachanarrachu28@gmail.com>
2020-11-02 23:09:51 +0530

    Added : Maximum out off 3 float numbers and test cases in each position

--- src/main/java/findingmaximum/FindingMaximum.java
+++ src/main/java/findingmaximum/FindingMaximum.java
@@ -5,6 +5,10 @@ public class FindingMaximum {
 	public int compare(Integer o1, Integer o2) {
 		return o1.compareTo(o2);
 	}
+	public float compare(Float o1, Float o2) {
+		return o1.compareTo(o2);
+	}
+
 
 	public static int maxInteger(Integer a, Integer b, Integer c) {
 		int max = a;
@@ -15,8 +19,17 @@ public class FindingMaximum {
 			max = c;
 		return max;
 	}
+	public static float maxFloat(Float a, Float b, Float c) {
+		float max = a;
+		if (b.compareTo(a) > 0.0)
+			max = b;
+		if (c.compareTo(b) > 0.0)
+			max = c;
+		return max;
+	}
 public static void main(String[] args) {
 	System.out.println("****WELCOME TO FIND MAXIMUM AMONG GIVEN SET****");
 	System.out.println("Maximum integer" +maxInteger(1000,400,22));
+	System.out.println("Maximum float" +maxFloat(2.3f,7.8f,9.0f));
 }
 }
--- src/test/java/findingmaximum/TestFindingMaximum.java
+++ src/test/java/findingmaximum/TestFindingMaximum.java
@@ -28,5 +28,21 @@ public void testMaximumAtThirdPosition_testAssertionHappy() {
 	int result = maximum.maxInteger(1000, 267, 2987);
 	Assert.assertEquals(2987,result);
 }
+@Test
+public void testMaximumAtFirstPositionForFloat_testAssertionHappy() {
+	float result = maximum.maxFloat(100.2f, 22.256f, 35.6f);
+	Assert.assertEquals(result, 100.00, 0.25);
+}
+@Test
+public void testMaximumAtSecondPositionForFloat_testAssertionHappy() {
+	float result = maximum.maxFloat(10.0f, 222.256f, 35.6f);
+	Assert.assertEquals(result, 222.006, 0.25);
+}
+
+@Test
+public void testMaximumAtThirdPositionForFloat_testAssertionHappy() {
+	float result = maximum.maxFloat(10.0f, 22.2f, 35.6f);
+	Assert.assertEquals(result, 35.00, 0.6);
+}
 
 }
